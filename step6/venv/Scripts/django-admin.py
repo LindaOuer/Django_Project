3b16c84f0a0172534a4ba7c6629a0d1d27d98a89
LindaOuer <linda.ouerfelli@gmail.com>
2020-04-02 00:10:46 +0100

    Remove directory6

--- step6/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\Esprit\2019-2020\Semestre 2\4TWIN Python\Django_Project\Django_Project\step4\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

1349d1cef661aeaacd0ba320f44b49161eb19028
LindaOuer <linda.ouerfelli@gmail.com>
2020-04-17 08:41:26 +0100

    Revert "First test: JSON Parsing"
    This reverts commit 62a63bccbf4bd1aefd61d6a501daeb92b49d4732.

--- step11_DjangoRESTFramework/innovationHub/API/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from hub.models import Student
-
-
-class StudentSerializer(serializers.Serializer):
-    lastName = serializers.CharField(max_length=30)
-    firstName = serializers.CharField(max_length=30)
-    email = serializers.EmailField()
--- step11_DjangoRESTFramework/innovationHub/API/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('students/', views.students_List),
-]
